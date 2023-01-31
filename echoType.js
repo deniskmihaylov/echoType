9996680fac5bad5f8b74e92f60c28c8ad96ef3a5
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2023-01-31 19:18:11 +0200

    Add files via upload

---
+++ echoType.js
@@ -0,0 +1,10 @@
+function solve (param) {
+  console.log(typeof param)
+  if (typeof (param) === 'string' || typeof (param) === 'number') {
+    console.log(param)
+  } else {
+    console.log('Parameter is not suitable for printing')
+  }
+}
+
+solve (true)
